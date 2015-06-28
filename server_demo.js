03ec739b6e0fd266348b867dbdb3feaa4b197064
minseock <keunsol8247@naver.com>
2015-06-28 17:03:44 +0900

    Post added

---
+++ server_demo.js
@@ -0,0 +1,43 @@
+/**
+ * Created by minseock on 2015-06-28.
+ */
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+app.get('/',function (req,res) {
+    console.log("get");
+    res.send('');
+});
+app.post('/', function(req, res){
+    console.log(JSON.stringify(req.body));
+/*
+    authenticate(req.body.username, req.body.password, function(err, user){
+        if (user) {
+            // Regenerate session when signing in
+            // to prevent fixation
+            req.session.regenerate(function(){
+                // Store the user's primary key
+                // in the session store to be retrieved,
+                // or in this case the entire user object
+                req.session.user = user;
+                req.session.success = 'Authenticated as ' + user.name
+                    + ' click to <a href="/logout">logout</a>. '
+                    + ' You may now access <a href="/restricted">/restricted</a>.';
+                res.redirect('back');
+            });
+        } else {
+            req.session.error = 'Authentication failed, please check your '
+                + ' username and password.'
+                + ' (use "tj" and "foobar")';
+            res.redirect('login');
+        }
+    });
+    */
+});
+app.listen(3303,function() {
+    console.log('Server Start . ');
+});
