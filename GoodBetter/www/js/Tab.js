ca9df8bc935febce183ce3fd0076d82acd67919a
SungPhill Moon <sp9103@naver.com>
2015-06-30 21:26:49 +0900

    *머지 작업중
    멘붕

---
+++ GoodBetter/www/js/Tab.js
@@ -0,0 +1,15 @@
+
+
+/*document.addEventListener("deviceready", init, false);
+function init() {
+	alert("ddd");
+	//$("#Setting").css('display','block');
+}*/
+
+/*$("#Setting").click(function() {
+   if($(this).find('span').css('display') == 'none'){
+    $(this).find('span').css('display','block');
+   }else{
+    $(this).find('span').css('display','none');
+   }
+ });*/ 
