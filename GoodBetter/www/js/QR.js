0be55fb5eb3bf38fa6b4efa4707c774d73c856a9
minseock <keunsol8247@naver.com>
2015-07-07 17:20:18 +0900

    큐알코드 찍으면 서버에서 식물 정보가져오는 거 구현

--- GoodBetter/www/js/QR.js
+++ GoodBetter/www/js/QR.js
@@ -2,29 +2,48 @@ var resultDiv;
 
 document.addEventListener("deviceready", init, false);
 function init() {
-	
+
     //document.querySelector("#startScan").addEventListener("touchend", startScan, false);
     //resultDiv = document.querySelector("#results");
-	
-	startScan();
+
+    startScan();
 }
 
 function startScan() {
 
     cordova.plugins.barcodeScanner.scan(
         function (result) {
+            /*
             var s = "Result: " + result.text + "<br/>" +
-            "Format: " + result.format + "<br/>" +
-            "Cancelled: " + result.cancelled;
-			//resultDiv.innerHTML = s;
-			
-			alert(s);
-			
-			if(result.cancelled == true)
-				prevPage();
-			else
-				nextPage();
-        }, 
+                "Format: " + result.format + "<br/>" +
+                "Cancelled: " + result.cancelled;
+                */
+            //resultDiv.innerHTML = s;
+
+            $.ajax({
+                dataType: 'Json',
+                url: result.text,
+                success: function (data) {
+                    if(data.error == 0){
+                        var s = JSON.stringify(data.result);
+                        //alert(s);
+                        window.localStorage.setItem("cur_plant",s);
+                        nextPage();
+                    }
+                },
+                error: function (xhr, type) {
+                    //alert('server error occurred');
+                }
+            });
+            if (result.cancelled == true)
+                prevPage();
+            /*
+            if (result.cancelled == true)
+                prevPage();
+            else
+                nextPage();
+                */
+        },
         function (error) {
             alert("Scanning failed: " + error);
         }
@@ -33,13 +52,13 @@ function startScan() {
 }
 
 function nextPage() {
-	var dirPath = dirname(location.href);
+    var dirPath = dirname(location.href);
     fullPath = dirPath + "/INFO.html";
     window.location = fullPath;
 }
 
 function prevPage() {
-	var dirPath = dirname(location.href);
+    var dirPath = dirname(location.href);
     fullPath = dirPath + "/index.html";
     window.location = fullPath;
 }
