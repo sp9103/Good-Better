456081788fcbc4ebcb361d5d9cbbe92353198284
최민석 <min@choemineoguiMBP.lan>
2015-07-26 22:04:08 +0900

    수지 사진...

--- GoodBetter/www/js/PlantTalk.js
+++ GoodBetter/www/js/PlantTalk.js
@@ -18,6 +18,7 @@ function makeTalkingBubble(arrow, content)
     html +="<div class=\"popover__"+dir[arrow]+"-arrowMin\"></div>";
     html +="<div class=\"popover__contentMin\">";
     if(image) html +="<img style=\"max-width: 100%; height: auto;\" src=\""+image+"\">";
+    //if(image) html +="<img style=\"max-width: 100%; height: auto;\" src=\"http://cache.clien.net/cs2/data/file/park/20141106131055_XRc8k32w_31846318_gJ0phAdm_3.jpg\">";
     html +="<div style=\"text-align:center;opacity:0.4;\">";
     html +=content;
     html +="</div></div></div></div>";
