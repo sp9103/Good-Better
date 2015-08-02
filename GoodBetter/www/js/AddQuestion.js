2a3362e7edf88308fff23f7826245d3cf3547a42
최민석 <min@choeminseogui-MacBook-Pro.local>
2015-08-02 23:07:23 +0900

    텍스트형식 질문 올리는 기능 구현(사진 아직 안됨)

--- GoodBetter/www/js/AddQuestion.js
+++ GoodBetter/www/js/AddQuestion.js
@@ -1,8 +1,45 @@
 /**
  * Created by min on 2015. 8. 1..
  */
+
+document.querySelector("#QuestionCancel").addEventListener("touchend", QuestionCancelClick, false);
+
+function QuestionCancelClick() {
+
+    var dirPath = dirname(location.href);
+    fullPath = dirPath + "/INFO.html";
+    window.location = fullPath;
+    //TalkClick();
+}
+function dirname(path) {
+    return path.replace(/\\/g, '/').replace(/\/[^\/]*$/, '');
+}
+
 function submitClicked(content, image) {
     if(content) {
+        //alert(content);
+        var formData = new FormData();
+        formData.append('clientCode',2);
+        formData.append('plantCode',1);
+        formData.append('question',content);
+        if(image) formData.append('talkImage',image);
+
+        $.ajax({
+            url:'http://goodandbetter.cafe24.com/appRegisterTalk',
+            data:formData,
+            cache:false,
+            processData:false,
+            contentType:false,
+            type:'POST',
+            success:function (data,status,req) {
+                if(data.error == 0) alert('success');
+                else alert('error');
+            },
+            error:function (req,status,error) {
+                alert('fail');
+            }
+
+        });
 
     }else {
         alert("질문을 입력하세요!");
