76cfece897f1614ef65f27aadc4e37bd7201fb37
최민석 <min@choeminseogui-MacBook-Pro.local>
2015-08-03 21:38:58 +0900

    포스팅함수만 구현

--- GoodBetter/www/js/AddQuestion.js
+++ GoodBetter/www/js/AddQuestion.js
@@ -15,14 +15,21 @@ function dirname(path) {
     return path.replace(/\\/g, '/').replace(/\/[^\/]*$/, '');
 }
 
-function submitClicked(content, image) {
+function submitClicked(content, imageUri) {
     if(content) {
         //alert(content);
         var formData = new FormData();
         formData.append('clientCode',2);
         formData.append('plantCode',1);
         formData.append('question',content);
-        if(image) formData.append('talkImage',image);
+        /*
+        if(imageUri){
+            var blob = dataURItoBlob(imageUri);
+            formData.append('talkImage',blob);
+        }*/
+        var blob = dataURItoBlob('content://media/external/images/media/67072');
+        alert(""+blob.type);
+        formData.append('talkImage',blob);
 
         $.ajax({
             url:'http://goodandbetter.cafe24.com/appRegisterTalk',
@@ -33,7 +40,7 @@ function submitClicked(content, image) {
             type:'POST',
             success:function (data,status,req) {
                 if(data.error == 0) alert('success');
-                else alert('error');
+                else alert(data.errMSG);
             },
             error:function (req,status,error) {
                 alert('fail');
@@ -44,4 +51,24 @@ function submitClicked(content, image) {
     }else {
         alert("질문을 입력하세요!");
     }
+}
+
+function dataURItoBlob(dataURI) {
+    // convert base64/URLEncoded data component to raw binary data held in a string
+    var byteString;
+    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+        byteString = atob(dataURI.split(',')[1]);
+    else
+        byteString = unescape(dataURI.split(',')[1]);
+
+    // separate out the mime component
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+    // write the bytes of the string to a typed array
+    var ia = new Uint8Array(byteString.length);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+
+    return new Blob([ia], {type:mimeString});
 }
