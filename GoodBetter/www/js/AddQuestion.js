e86dc2e73711d3d1dfb7e1a92bb99295cef589a6
최민석 <min@choeminseogui-MacBook-Pro.local>
2015-08-04 22:55:50 +0900

    사진 찍어서 톡 올리는거 구현

--- GoodBetter/www/js/AddQuestion.js
+++ GoodBetter/www/js/AddQuestion.js
@@ -14,11 +14,20 @@ function QuestionCancelClick() {
 function dirname(path) {
     return path.replace(/\\/g, '/').replace(/\/[^\/]*$/, '');
 }
+function submitClicked(content) {
+    var imageUri = window.localStorage.getItem("imageuri");
+    if(!content){
+        alert("질문을 입력하세요!");
+    }else if(!imageUri){
+        submitClicked_NoImage(content);
+    }else {
+        submitClicked_WithImage(content,imageUri);
+    }
+}
 
-function submitClicked(content, imageUrir) {
-    if(content) {
+function submitClicked_WithImage(content, imageUri) {
 
-        imageUri = 'file:///storage/emulated/0/DCIM/Facebook/FB_IMG_1432276141336.jpg';
+        //imageUri = 'file:///storage/emulated/0/DCIM/Facebook/FB_IMG_1432276141336.jpg';
 
         var options = new FileUploadOptions();
         options.fileKey = "talkImage";
@@ -40,60 +49,30 @@ function submitClicked(content, imageUrir) {
             }, function(error) {
                 alert('fail');
             }, options);
-        /*
-        //alert(content);
-        var formData = new FormData();
-        formData.append('clientCode',2);
-        formData.append('plantCode',1);
-        formData.append('question',content);
 
-        if(imageUri){
-            var blob = dataURItoBlob(imageUri);
-            formData.append('talkImage',blob);
-        }
-        //var blob = dataURItoBlob('file:///storage/emulated/0/DCIM/Facebook/FB_IMG_1432276141336.jpg');
-        //alert(""+blob.type);
-        formData.append('talkImage',new File("file:///storage/emulated/0/DCIM/Facebook/FB_IMG_1432276141336.jpg"));
-
-        $.ajax({
-            url:'http://goodandbetter.cafe24.com/appRegisterTalk',
-            data:formData,
-            cache:false,
-            processData:false,
-            contentType:false,
-            type:'POST',
-            success:function (data,status,req) {
-                if(data.error == 0) alert('success');
-                else alert(data.errMSG);
-            },
-            error:function (req,status,error) {
-                alert('fail');
-            }
-
-        });
-        */
-
-    }else {
-        alert("질문을 입력하세요!");
-    }
 }
 
-function dataURItoBlob(dataURI) {
-    // convert base64/URLEncoded data component to raw binary data held in a string
-    var byteString;
-    if (dataURI.split(',')[0].indexOf('base64') >= 0)
-        byteString = atob(dataURI.split(',')[1]);
-    else
-        byteString = unescape(dataURI.split(',')[1]);
+function submitClicked_NoImage(content) {
+         var formData = new FormData();
+         formData.append('clientCode',2);
+         formData.append('plantCode',1);
+         formData.append('question',content);
 
-    // separate out the mime component
-    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+         $.ajax({
+         url:'http://goodandbetter.cafe24.com/appRegisterTalk',
+         data:formData,
+         cache:false,
+         processData:false,
+         contentType:false,
+         type:'POST',
+         success:function (data,status,req) {
+         if(data.error == 0) alert('success');
+         else alert(data.errMSG);
+         },
+         error:function (req,status,error) {
+         alert('fail');
+         }
 
-    // write the bytes of the string to a typed array
-    var ia = new Uint8Array(byteString.length);
-    for (var i = 0; i < byteString.length; i++) {
-        ia[i] = byteString.charCodeAt(i);
-    }
-
-    return new Blob([ia], {type:mimeString});
+         });
 }
+
