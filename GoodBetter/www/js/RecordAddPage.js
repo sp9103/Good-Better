7a5652b71319d2a690de9d5f36c9266c8ea8d0f9
SungPhill Moon <sp9103@naver.com>
2015-08-06 18:59:25 +0900

    *체크리스트 업로드 (without Image) 구현중.
    -전송 디버깅중

--- GoodBetter/www/js/RecordAddPage.js
+++ GoodBetter/www/js/RecordAddPage.js
@@ -18,6 +18,51 @@ function dirname(path) {
     return path.replace(/\\/g, '/').replace(/\/[^\/]*$/, '');
 }
 
+function SubmitClicked(){
+	var imageUri = window.localStorage.getItem("imageuri");
+			
+	//이미지 없을때
+	if(!imageUri){
+		var formData = new FormData();
+		
+		var cur_plant = window.localStorage.getItem("cur_plant");
+		var plant = JSON.parse(cur_plant);
+		
+		formData.append('clientCode',2);			//임시
+		formData.append('plantCode', 1);				//임시
+		formData.append('water',document.getElementById("Water").checked ? 1 : 0);
+		formData.append('supplements',document.getElementById("Supplements").checked ? 1 : 0);
+		formData.append('damage',document.getElementById("Damage").checked ? 1 : 0);
+		formData.append('pruning',document.getElementById("Pruning").checked ? 1 : 0);
+		formData.append('cleanleaf',document.getElementById("Cleanleaf").checked ? 1 : 0);
+		formData.append('repotting',document.getElementById("Repotting").checked ? 1 : 0);
+		formData.append('change',document.getElementById("Change").checked ? 1 : 0);
+		formData.append('text',document.getElementById("CHETEXT").value);
+		
+		$.ajax({
+			url:'http://goodandbetter.cafe24.com/appUpdateChecklist',
+			data:formData,
+			cache:false,
+			processData:false,
+			contentType:false,
+			type:'POST',
+			success:function (data,status,req) {
+				if(data.error == 0){
+					//이번 페이지로 돌아가기 구현
+					alert("체크리스트가 등록되었습니다.");
+					CancelClick()
+				} 
+				else alert(data.errMSG);
+			},
+			error:function (req,status,error) {
+				alert('fail');
+			}
+		});
+		alert("upload complete!");
+	}else{
+		
+	}	
+}
  
 
 
