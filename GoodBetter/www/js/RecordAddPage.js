e196c7c34f6ecc6dce888cd055afbb2d517c692e
최민석 <min@choeminseogui-MacBook-Pro.local>
2015-08-08 20:34:48 +0900

    체크리스트 사진도 올라감

--- GoodBetter/www/js/RecordAddPage.js
+++ GoodBetter/www/js/RecordAddPage.js
@@ -19,73 +19,92 @@ function dirname(path) {
     return path.replace(/\\/g, '/').replace(/\/[^\/]*$/, '');
 }
 
-function SubmitClicked(){
+function SubmitClicked() {
 	var imageUri = window.localStorage.getItem("imageuri");
+	if(!imageUri){
+		submitClicked_NoImage();
+	}else {
+		submitClicked_WithImage(imageUri);
+	}
+}
+
+function submitClicked_WithImage(imageUri) {
+
+
 	var cur_plant = window.localStorage.getItem("cur_plant");
 	var plant = JSON.parse(cur_plant);
-	var clientCode = window.localStorage.getItem("clientCode");	
-			
-	//이미지 없을때
-	if(!imageUri){
-		var formData = new FormData();
-		
-		formData.append('clientCode',clientCode);				//임시
-		formData.append('plantCode', plant.PLA_Code);				//임시
-		formData.append('water',document.getElementById("Water").checked ? 1 : 0);
-		formData.append('supplements',document.getElementById("Supplements").checked ? 1 : 0);
-		formData.append('damage',document.getElementById("Damage").checked ? 1 : 0);
-		formData.append('pruning',document.getElementById("Pruning").checked ? 1 : 0);
-		formData.append('cleanleaf',document.getElementById("Cleanleaf").checked ? 1 : 0);
-		formData.append('repotting',document.getElementById("Repotting").checked ? 1 : 0);
-		formData.append('change',document.getElementById("Change").checked ? 1 : 0);
-		formData.append('text',document.getElementById("CHETEXT").value);
-		
-		$.ajax({
-			url:'http://goodandbetter.cafe24.com/appUpdateChecklist',
-			data:formData,
-			cache:false,
-			processData:false,
-			contentType:false,
-			type:'POST',
-			success:function (data,status,req) {
-				if(data.error == 0){
-					//이번 페이지로 돌아가기 구현
-					alert("체크리스트가 등록되었습니다.");
-					CancelClick();
-				} 
-				else alert(data.errMSG);
-			},
-			error:function (req,status,error) {
-				alert('fail');
-			}
-		});
-	}else{
-		/*var options = new FileUploadOptions();
-		
-		options.fileKey = "checklistImage";
-        options.fileName = imageUri.substr(imageUri.lastIndexOf('/') + 1);
-		var mime=imageUri.substr(imageUri.lastIndexOf('.') + 1);
-		var type;
-		if(mime=='jpg' || mime=='jpeg') type='image/jpeg';
-		else if(mime=='png') type = 'image/png';
-        options.mimeType = type;
-		
-		var params = {};
-        params.clientCode = clientCode;
-        params.plantCode = plant.PLA_Code;
-		params.water = document.getElementById("Water").checked ? 1 : 0;
-        formData.append('plantCode', plant.PLA_Code);				//임시
-		formData.append('water',document.getElementById("Water").checked ? 1 : 0);
-		formData.append('supplements',document.getElementById("Supplements").checked ? 1 : 0);
-		formData.append('damage',document.getElementById("Damage").checked ? 1 : 0);
-		formData.append('pruning',document.getElementById("Pruning").checked ? 1 : 0);
-		formData.append('cleanleaf',document.getElementById("Cleanleaf").checked ? 1 : 0);
-		formData.append('repotting',document.getElementById("Repotting").checked ? 1 : 0);
-		formData.append('change',document.getElementById("Change").checked ? 1 : 0);
-		formData.append('text',document.getElementById("CHETEXT").value);
+	var clientCode = window.localStorage.getItem("clientCode");
 
-        options.params = params;*/
-	}
+	var options = new FileUploadOptions();
+	options.fileKey = "checklistImage";
+	options.fileName = imageUri.substr(imageUri.lastIndexOf('/') + 1);
+	var mime=imageUri.substr(imageUri.lastIndexOf('.') + 1);
+	var type;
+	if(mime=='jpg' || mime=='jpeg') type='image/jpeg';
+	else if(mime=='png') type = 'image/png';
+	options.mimeType = type;
+
+	var params = {};
+	params.clientCode = clientCode;
+	params.plantCode = plant.PLA_Code;
+	params.water = document.getElementById("Water").checked ? 1 : 0;
+	params.supplements = document.getElementById("Supplements").checked ? 1 : 0;
+	params.damage = document.getElementById("Damage").checked ? 1 : 0;
+	params.pruning = document.getElementById("Pruning").checked ? 1 : 0;
+	params.cleanleaf = document.getElementById("Cleanleaf").checked ? 1 : 0;
+	params.repotting = document.getElementById("Repotting").checked ? 1 : 0;
+	params.change = document.getElementById("Change").checked ? 1 : 0;
+	params.text = document.getElementById("CHETEXT").value;
+
+	options.params = params;
+
+	var ft = new FileTransfer();
+	ft.upload(imageUri, encodeURI('http://goodandbetter.cafe24.com/appUpdateCheckList'),
+		function(data) {
+			alert("체크리스트가 등록되었습니다.");
+			CancelClick();
+		}, function(error) {
+			alert('fail');
+		}, options);
+}
+
+function submitClicked_NoImage() {
+	var cur_plant = window.localStorage.getItem("cur_plant");
+	var plant = JSON.parse(cur_plant);
+	var clientCode = window.localStorage.getItem("clientCode");
+
+	var formData = new FormData();
+
+	formData.append('clientCode',clientCode);				//임시
+	formData.append('plantCode', plant.PLA_Code);				//임시
+	formData.append('water',document.getElementById("Water").checked ? 1 : 0);
+	formData.append('supplements',document.getElementById("Supplements").checked ? 1 : 0);
+	formData.append('damage',document.getElementById("Damage").checked ? 1 : 0);
+	formData.append('pruning',document.getElementById("Pruning").checked ? 1 : 0);
+	formData.append('cleanleaf',document.getElementById("Cleanleaf").checked ? 1 : 0);
+	formData.append('repotting',document.getElementById("Repotting").checked ? 1 : 0);
+	formData.append('change',document.getElementById("Change").checked ? 1 : 0);
+	formData.append('text',document.getElementById("CHETEXT").value);
+
+	$.ajax({
+		url:'http://goodandbetter.cafe24.com/appUpdateChecklist',
+		data:formData,
+		cache:false,
+		processData:false,
+		contentType:false,
+		type:'POST',
+		success:function (data,status,req) {
+			if(data.error == 0){
+				//이번 페이지로 돌아가기 구현
+				alert("체크리스트가 등록되었습니다.");
+				CancelClick();
+			}
+			else alert(data.errMSG);
+		},
+		error:function (req,status,error) {
+			alert('fail');
+		}
+	});
 }
  
 
