b6e9a75754bd1f2fe56dc0a50f64864463482fc9
최민석 <min@choeminseogui-MacBook-Pro.local>
2015-08-11 02:22:52 +0900

    아이콘 작업 완료

--- GoodBetter/platforms/ios/www/js/AddQuestion.js
+++ GoodBetter/platforms/ios/www/js/AddQuestion.js
@@ -15,7 +15,7 @@ function dirname(path) {
     return path.replace(/\\/g, '/').replace(/\/[^\/]*$/, '');
 }
 function submitClicked(content) {
-    document.getElementById("SubmitButton").innerHTML = "질문을 등록하고 있습니다.";
+    document.getElementById("SubmitButton").innerHTML = "<button class=\"Submit Submit--large\">질문을 등록하고 있습니다.</button>";
     document.getElementById("SubmitButton").style.opacity = 0.3;
     document.getElementById("SubmitButton").style.pointerEvents = 'none';
     var imageUri = window.localStorage.getItem("imageuri");
@@ -58,7 +58,7 @@ function submitClicked_WithImage(content, imageUri) {
                 alert("질문이 등록되었습니다.");
 				QuestionCancelClick();
             }, function(error) {
-                document.getElementById("SubmitButton").innerHTML = "질문하기";
+                document.getElementById("SubmitButton").innerHTML = "<button class=\"Submit Submit--large\" onclick=\"SubmitClicked()\">질문하기</button>";
                 document.getElementById("SubmitButton").style.opacity = 1.0;
                 document.getElementById("SubmitButton").style.pointerEvents = '';
                 alert('네트워크 상태를 확인해주세요.');
@@ -92,7 +92,7 @@ function submitClicked_NoImage(content) {
          else alert(data.errMSG);
          },
          error:function (req,status,error) {
-             document.getElementById("SubmitButton").innerHTML = "질문하기";
+             document.gtElementById("SubmitButton").innerHTML = "<button class=\"Submit Submit--large\" onclick=\"SubmitClicked()\">질문하기</button>";
              document.getElementById("SubmitButton").style.opacity = 1.0;
              document.getElementById("SubmitButton").style.pointerEvents = '';
              alert('네트워크 상태를 확인해주세요.');
